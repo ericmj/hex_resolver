59fcc8bcee1cd13e0e5b8e596ce65c3f278af30b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-02-07 02:09:22 +0100

    mix new

---
+++ lib/hex_resolver.ex
@@ -0,0 +1,18 @@
+defmodule HexResolver do
+  @moduledoc """
+  Documentation for `HexResolver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HexResolver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hex_resolver_test.exs
@@ -0,0 +1,8 @@
+defmodule HexResolverTest do
+  use ExUnit.Case
+  doctest HexResolver
+
+  test "greets the world" do
+    assert HexResolver.hello() == :world
+  end
+end
